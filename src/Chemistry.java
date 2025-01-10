bc0220ff950fd8b813399a5e437a3609a1f5a2d1
BT_1N3_32 <sample@bths.edu>
2025-01-10 13:35:18 -0500

    initial

---
+++ src/Chemistry.java
@@ -0,0 +1,5 @@
+public class Chemistry {
+    public static void main(String[] args) {
+        System.out.println("I don't like chemistry even more!");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("our first shared repo");
         System.out.println("hi ");
+        System.out.println("I don't like chemistry!!!!!!!!!");
     }
 }
